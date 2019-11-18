a94e01da78e450fd19bb316386198daca0903538
TheDoctorOne <mahmudkocas@gmail.com>
2019-11-18 04:18:08 +0300

    Done FileOperations. Not tested.

--- src/main/java/io/github/thedoctorone/sboot/Service/FileOperations.java
+++ src/main/java/io/github/thedoctorone/sboot/Service/FileOperations.java
@@ -1,46 +1,57 @@
 package io.github.thedoctorone.sboot.Service;
 
 import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
-import org.hibernate.validator.internal.util.privilegedactions.GetDeclaredConstructor;
-import org.hibernate.validator.internal.util.privilegedactions.GetDeclaredConstructors;
-
 /**
  * Writes & Reads the Object
+ * 
  * @param <E> is the class you want to write & read
  */
-public abstract class FileOperations<E> {
-    
+public abstract class FileOperations<E> implements Serializable {
+
+    private static final long serialVersionUID = 1L;
     private String path;
 
     /**
      * Set the file path.
+     * 
      * @param path - File Path
      */
-    public void setFilePath(String path) {
-        if(path.endsWith("/") || path.endsWith("\\")) {
+    public void setFilePath(@NotEmpty String path) {
+        if (path.endsWith("/") || path.endsWith("\\")) {
             path = path.substring(0, path.length() - 2);
         }
         this.path = path;
     }
 
     /**
+     * Returns path
+     * 
+     * @return - path
+     */
+    public String getFilePath() {
+        return path;
+    }
+
+    /**
      * @param object
      * @param filename - name of the file
-     * @param endfix - endfix of the file, ex: ".dat"
+     * @param suffix   - suffix of the file, ex: ".dat"
      */
-    public void write(@NotNull Object object,@NotEmpty String filename,@NotEmpty String endfix) throws IOException {
-        FileOutputStream fos = new FileOutputStream(new File(path + "/" + filename + endfix));
+    public void write(@NotNull Object object, @NotEmpty String filename, @NotEmpty String suffix) throws IOException {
+        FileOutputStream fos = new FileOutputStream(new File(path + "/" + filename + suffix));
         ObjectOutputStream oos = new ObjectOutputStream(fos);
         oos.writeObject(object);
         oos.close();
@@ -48,26 +59,27 @@ public abstract class FileOperations<E> {
     }
 
     /**
-     * Filename will be set to timestamp
-     * EndFix will be the class name
+     * Filename will be set to timestamp suffix will be the class name
      */
     public void write(@NotNull E object) throws IOException {
         write(object, Calendar.getInstance().getTimeInMillis() + "", object.getClass().getName());
     }
+
     /**
-     * @param list of the objects
+     * @param list     of the objects - can not be empty
      * @param filename - name of the file
-     * @param endfix - Endfix of the file
+     * @param suffix   - suffix of the file
      * @throws IOException may occur
      */
-    public void writeAsList(@NotEmpty List<E> list,@NotEmpty String filename,@NotEmpty String endfix) throws IOException {
-        write(list, filename, endfix);
+    public void writeAsList(@NotEmpty List<E> list, @NotEmpty String filename, @NotEmpty String suffix)
+            throws IOException {
+        write(list, filename, suffix);
     }
 
     /**
-     * @param list of the objects
+     * @param list     of the objects - can not be empty
      * @param filename - name of the file
-     * @param endfix - Endfix will be ".list"
+     * @param suffix   - suffix will be ".list"
      * @throws IOException may occur
      */
     public void writeAsList(@NotEmpty List<E> list, String filename) throws IOException {
@@ -75,24 +87,90 @@ public abstract class FileOperations<E> {
     }
 
     /**
-     * @param list of the objects
+     * @param list     of the objects - can not be empty
      * @param filename - is class name
-     * @param endfix - Endfix will be ".list"
+     * @param suffix   - suffix will be ".list"
      * @throws IOException may occur
      */
     public void writeAsList(@NotEmpty List<E> list) throws IOException {
-        write(list, list.get(0).getClass().getName() , ".list");
+        write(list, list.get(0).getClass().getName(), ".list");
     }
 
-    public E read(@NotEmpty String filename) {
-        return null;
+    /**
+     * Reads the file according to filename
+     * 
+     * @param filename - name of the file
+     * @return the object
+     * @throws IOException
+     * @throws ClassNotFoundException
+     */
+    @SuppressWarnings("unchecked")
+    public E read(@NotEmpty String filename) throws IOException, ClassNotFoundException {
+        E object = null;
+        FileInputStream fis = new FileInputStream(new File(path + "/" + filename));
+        ObjectInputStream ois = new ObjectInputStream(fis);
+        object = (E) ois.readObject();
+        ois.close();
+        fis.close();
+        return object;
     }
 
-    public List<E> readAsList(@NotEmpty String filename) {
-        return null;
+    @SuppressWarnings("unchecked")
+    public List<E> readAsList(@NotEmpty String filename) throws IOException, ClassNotFoundException {
+        List<E> list = new ArrayList<>();
+        FileInputStream fis = new FileInputStream(new File(path + "/" + filename));
+        ObjectInputStream ois = new ObjectInputStream(fis);
+        list = (List<E>) ois.readObject();
+        ois.close();
+        fis.close();
+        return list;
     }
 
-    public List<E> readAll() {
-        return null;
+    /**
+     * Reads the directory according to path, use it wisely!
+     * 
+     * @return - list of objects
+     * @throws IOException
+     * @throws ClassNotFoundException
+     */
+    @SuppressWarnings("unchecked")
+    public List<E> readDirectory() throws ClassNotFoundException, IOException {
+        List<E> list = new ArrayList<>();
+        File directory = new File(path); 
+        if(directory.isDirectory()) {
+            for(File file : directory.listFiles()) {
+                FileInputStream fis = new FileInputStream(file);
+                ObjectInputStream ois = new ObjectInputStream(fis);
+                list.add((E) ois.readObject());
+                ois.close();
+                fis.close();
+            }
+        }
+        return list;
+    }
+
+    /**
+     * Reads the files at the directory according to their suffix 
+     * @param suffix
+     * @return - list of objects
+     * @throws ClassNotFoundException
+     * @throws IOException
+     */
+    @SuppressWarnings("unchecked")
+    public List<E> readDirectory(@NotEmpty String suffix) throws ClassNotFoundException, IOException {
+        List<E> list = new ArrayList<>();
+        File directory = new File(path); 
+        if(directory.isDirectory()) {
+            for(File file : directory.listFiles()) {
+                if(file.getName().endsWith(suffix)) {
+                    FileInputStream fis = new FileInputStream(file);
+                    ObjectInputStream ois = new ObjectInputStream(fis);
+                    list.add((E) ois.readObject());
+                    ois.close();
+                    fis.close();
+                }
+            }
+        }
+        return list;
     }
 }
